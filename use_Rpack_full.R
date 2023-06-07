fb13130be0b3115f24eba6ff347390eef75f6713
mathiasblicher1991 <mathiasblicher1991@gmail.com>
2023-06-07 16:59:02 +0200

    Test scripts for new datasets
    Added test scripts (clones of roxy.R) for each individual built-in dataset.
    
    This commit also updates the .Rd documentation for two plot functions, which is fine I believe

---
+++ use_Rpack_full.R
@@ -0,0 +1,61 @@
+# INSTALL R PACKAGE
+setwd("~/Documents/GitHub/RP-RES-forecast-evaluation/")
+
+library(devtools)
+library(roxygen2)
+
+# Update package documentation
+document(pkg = ".")
+
+# Install from local repository
+install(".")
+
+# ======================================= #
+# ------------ INITIAL STUFF ------------ #
+# ======================================= #
+
+# Load Package
+library(IEAwind51RP)
+
+# Load data
+data("IEAW51-ReadingExample-full")
+fc_obs_data <- sample_may23_full
+rm(sample_may23_full)
+
+# Summary statistics
+summaryStats(fc_obs_data)
+
+# Plot
+ti <- 1200:1270
+for(i in 1:3){
+  f <- fc_obs_data$forecasts[[i]]
+  quantilePlot(f[ti,-c(1:2)],x=f$TimeStamp[ti],main=paste0("Forecast ",LETTERS[i]),ylab = "Normalized Wind Power",ylim=c(0,1))
+  lines(fc_obs_data$observations$TimeStamp,fc_obs_data$observations$obs)
+}
+
+# ======================================= #
+# --------- FORECAST EVALUATION --------- #
+# ======================================= #
+
+# CRPS etc.
+forecastEvaluation(fc_obs_data)
+
+# Restrict data to intersecting timestamps only
+fc_obs_data_eval <- evaluationSet(fc_obs_data)
+
+# Compute event detection tables for all forecast series (NB: takes time at the moment!)
+detect_table_list <- eventDetectionTable(fc_obs_data_eval,change=-0.01,window=6)
+
+# Make contingency tables
+contingency_table <- contingencyTableList(detect_table_list,threshold = 5)
+
+names(contingency_table) <- c("Forecast A","Forecast B","Forecast C")
+
+# Print contingency table
+printContingencyTable(contingency_table)
+
+# ROC curves
+for(i in 1:3){
+  f=i
+  roc <- rocCurve(detect_table_list[[f]],main=paste0("Forecast ",LETTERS[f]))
+}
