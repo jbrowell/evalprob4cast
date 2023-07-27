8711f2d056278cd158362c158be151921a8c646e
mathiasblicher1991 <mathiasblicher1991@gmail.com>
2023-07-27 18:51:55 +0200

    Update rocCurve.R
    Added an exception for the special case where no separate points on the ROC curve can be computed.

--- R/rocCurve.R
+++ R/rocCurve.R
@@ -10,14 +10,25 @@ rocCurve <- function(detect_table,threshold=5,main=""){
   
   M_eval <- dim(detect_table)[2]-2
   detect_table_sum <- data.frame(obs=detect_table[,2],
-                                 forecast=apply(detect_table[,-c(1,2)],1,function(x){sum(x)/M_eval}))
+                                 forecast=apply(as.matrix(detect_table[,-c(1,2)]),1,function(x){sum(x)/M_eval}))
   
   # Get binary table for obs vs. forecast (currently 5 positives needed for a detection)
-  detect_table_ct <- detectToBinary(detect_table_sum,threshold=5/M_eval)
+  detect_table_ct <- detectToBinary(detect_table_sum,threshold=threshold/M_eval)
 
-  # Plot ROC curve
+  # Prepare ROC curve object
   PRROC_obj <- roc.curve(scores.class0 = detect_table_sum[,2],weights.class0 = detect_table_sum[,1],curve=T)
-  plot(PRROC_obj,main=main)
+  
+  # Check that points can be drawn
+  if( sum(abs(diff(PRROC_obj$curve[,3])))==0 ){
+    
+    plot(0,0,type="n",xaxt="n",yaxt="n",xlab="",ylab="",main=main)
+    text(0,0,"This forecast can not be converted into a ROC curve.",cex=1.3)
+    
+  }else{
+    
+    plot(PRROC_obj,main=main)
+    
+  }
   
   return(PRROC_obj)
   
