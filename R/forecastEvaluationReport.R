469f899e0570a9f1eb8291e51f5f745f5c0510cc
mathiasblicher1991 <mathiasblicher1991@gmail.com>
2023-09-06 14:59:13 +0200

    Update forecastEvaluationReport.R

--- R/forecastEvaluationReport.R
+++ R/forecastEvaluationReport.R
@@ -4,27 +4,113 @@
 #' @param dest Destination for the compiled HTML report.
 #' @return Currently nothing.
 #' @export
-forecastEvaluationReport <- function(data,dest="./",options=NULL) {
+forecastEvaluationReport <- function(data,dest="./",options=NULL,delete_source=T) {
   
   # Filenames
   output_filename <- paste0(dest,"forecast_evaluation_report.html")
-  rmd_name <- paste0(dest,"rmd_",as.numeric(Sys.time()),".R")
+  rmd_name <- paste0(dest,"rmd_",as.numeric(Sys.time()),".Rmd")
+  
+  # Local writing function
+  addline <- function(x){cat(paste0(x,"\n"),file=rmd_name,append=T)}
+  BR <- function(){cat("\n",file=rmd_name,append=T)}
+  addchunk <- function(x,echo=T,include=T,message=T,results='show',fig.keep='all'){
+    addline(paste0("```{r, echo=",echo,", include=",include,", message=",message,"}"))
+    addline(x)
+    addline("```\n")
+  }
   
   # -------------------------- #
   # ----- REPORT CONTENT ----- #
   # -------------------------- #
   
+  # Title (static now, customizable later)
+  addline("---")
+  addline("title: \"IEA Forecast Evaluation\"")
+  addline("author: \"Recommended Practice R Package\"")
+  addline("date: \"`r format(Sys.time(), '%d %B, %Y')`\"")
+  addline("output: html_document")
+  addline("---")
+  
+  # Fundamentals
+  Nfcc <- length(data$forecasts)
+  fcnames <- names(data$forecasts)
+  
   # Summary statistics
-  cat("#' ## Summary statistics\n",file = rmd_name,append = T)
-  cat("summaryStats(data)\n",file = rmd_name,append = T)
+  addline("## Summary Statistics")
+  addchunk("summaryStats(data)",echo = F)
+  
+  # Visualized forecasts
+  addline("## Visualized Forecasts")
+  addchunk("
+  ti <- 1:100
+  for(i in 1:Nfcc){
+    f <- data$forecasts[[i]]
+    
+    if(dim(f)[1]<length(ti)){
+      ti <- 1:dim(f)[1]
+    }
+    
+    quantilePlot(Filter(is.numeric,f)[ti,],x=f$TimeStamp[ti],main=fcnames[i],ylab = \"Quantity\")
+    lines(data$observations$TimeStamp,data$observations$obs)
+  }",echo=F)
   
   # CRPS
-  cat("#' ## CRPS\n",file = rmd_name,append = T)
-  cat("#' ### CRPS \"blind\"\n",file = rmd_name,append = T)
-  cat("forecastEvaluation(data,by_lead_time = F)\n",file = rmd_name,append = T)
-  cat("#' ### CRPS by lead time\n",file = rmd_name,append = T)
-  cat("forecastEvaluation(data,by_lead_time = T)\n",file = rmd_name,append = T)
+  addline("## CRPS - Basic")
+  addchunk("forecastEvaluation(data,by_lead_time = F)",echo=F)
+  addline("## CRPS - By Lead Time")
+  addchunk("forecastEvaluation(data,by_lead_time = T)",echo=F,message=F)
   
+  # EVENT DETECTION RELATED MATTERS
+  
+  # Parameters
+  event_change <- -0.03
+  event_window <- 6
+  contingency_threshold <- 0.2
+
+  # Restrict data to intersecting timestamps only
+  data_eval <- evaluationSet(data)
+  
+  # Compute event detection tables for all forecast series (NB: takes time at the moment!)
+  detect_table_list <- eventDetectionTable(data_eval,
+                                           change=event_change,
+                                           window=event_window)
+  
+  # Make contingency tables
+  contingency_table <- contingencyTableList(detect_table_list,threshold = contingency_threshold)
+  
+  # Print contingency table
+  addline("## Contingency Table")
+  addline(paste0("Change = ",event_change))
+  BR()
+  addline(paste0("Window = ",event_window))
+  BR()
+  addline(paste0("Threshold = ",contingency_threshold))
+  BR()
+  addchunk("printContingencyTable(contingency_table)",echo=F)
+  
+  # ROC curves
+  addline("## ROC Curve")
+  addline("```{r, echo=F,message=F,results='hide'}")
+  addline("rocCurveList(detect_table_list)")
+  addline("```\n")
+  
+  #addchunk("rocCurveList(detect_table_list)",echo=F,message=F)
+  
+  # Brier scores
+  addline("## Brier Score")
+  probability_table <- lapply(detect_table_list,function(x){data.frame(TimeStamp=x$TimeStamp,
+                                                                       obs=x$obs,
+                                                                       prob=rowSums(as.matrix(x[,-c(1,2)]))/(dim(x)[2]-2))})
+  addchunk("unlist(lapply(probability_table,function(x){brierscore(x$prob,x$obs)}))",echo=F)
+  
+  # Reliability diagrams
+  addline("## Reliability Diagram")
+  addchunk("lapply(probability_table,function(x){
+    suppressWarnings(as.reliabilitydiag(x$prob,x$obs))
+    })
+  #",echo=F,message=F)
+  
+  # Can add ,xvalues=seq(0.05,0.95,by=0.1) after x$obs
   
   # -------------------------- #
   # ----- RENDER REPORT ------ #
@@ -38,5 +124,7 @@ forecastEvaluationReport <- function(data,dest="./",options=NULL) {
     #),
     output_file = output_filename
   )
+  
+  if(delete_source){file.remove(rmd_name)}
   
 }
