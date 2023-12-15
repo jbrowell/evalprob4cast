a067ce58334fd7ef2e14138df9a7a0210c94298a
mathiasblicher1991 <mathiasblicher1991@gmail.com>
2023-12-15 17:58:45 +0100

    Added rank histogram function
    + Added rank histogram
    + Custom number of bins can be chosen. Source: https://arxiv.org/pdf/2005.09018.pdf

---
+++ R/rankHistogram.R
@@ -0,0 +1,35 @@
+#' Rank histogram of a series of ensemble forecasts w.r.t a series of observations.
+#'
+#' @param f Vector representing a series of ensemble forecasts.
+#' @param y The corresponding observation series.
+#' @param nbins The desired number of bins. Defaults to m+1 bins (code 0)
+#' @param seed Optional seed. Defaults to 0 (random seed every time)
+#' @return Rank histogram
+#' @export
+
+rankHistogram <- function(f,y,nbins=0,seed=0){
+  
+  m <- dim(f)[2]
+  if(nbins==0){
+    nbins <- m + 1
+  }
+  
+  pair_matrix <- cbind(y,f)
+  
+  r <- unlist(apply(pair_matrix,1,function(x){
+    which(sort(x)==x[1])[max(1,round(length(which(sort(x)==x[1]))/2))]
+  }))
+  attr(r,"names") <- NULL
+  
+  if(seed==0){
+    set.seed(as.numeric(Sys.time()))
+  }else{
+    set.seed(seed)
+  }
+  
+  r_transformed <- (r - 1 + runif(length(r))) / (m+1)
+  rhist <- hist(r_transformed,breaks=nbins)
+  
+  return(invisible(rhist))
+  
+}
