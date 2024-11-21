f8ab777b71d490d4553a47744d763d7b5ea3d9d0
mathiasblicher1991 <mathiasblicher1991@gmail.com>
2024-11-21 13:13:11 +0100

    Small documentation update
    Updated documentation for VarS and LogS.

--- R/log_score.R
+++ R/log_score.R
@@ -1,6 +1,6 @@
 #' LogS (univariate) of a probabilistic forecast w.r.t an observation.
 #'
-#' @param sim Vector representing a probabilistic forecast.
+#' @param f Vector representing a probabilistic forecast.
 #' @param y The corresponding observation.
 #' @return Logarithmic score (LogS) of f and y.
 #' @export
